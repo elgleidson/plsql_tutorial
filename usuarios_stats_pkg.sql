97b3588e1df14b6f4c408225d380dc97a517f98a
Gleidson Echeli <el.gleidson@gmail.com>
2015-06-28 13:07:14 -0300

    Parte 3 do tutorial

--- usuarios_stats_pkg.sql
+++ usuarios_stats_pkg.sql
@@ -4,45 +4,53 @@ create or replace package plsql_tutorial.usuarios_stats as
   
   procedure audit_vendas(p_usuario_id in number);
   procedure recalc_vendas(p_usuario_id in number);
+  procedure audit_ligacoes(p_usuario_id in number);
+  procedure recalc_ligacoes(p_usuario_id in number);
   
 end;
 /
 
 create or replace package body plsql_tutorial.usuarios_stats as
 
-  function get_vendas_calc(p_usuario_id in number, o_ultimo_id out number) return number
+  function get_calc(p_tabela in varchar, p_coluna in varchar, p_usuario_id in number, o_ultimo_id out number) return number
   is
     v_valor number := 0;
   begin
-    select count(v.id), max(v.id) into v_valor, o_ultimo_id from vendas v where v.vendedor_id = p_usuario_id;
+    execute immediate 'select count(id), max(id) from '||p_tabela||' where '||p_coluna||' = :id'
+    into v_valor, o_ultimo_id
+    using p_usuario_id;
     
     return v_valor;
   end;
   
-  
-  procedure trata_vendas_divergentes(p_usuario_id in number, p_log_name in varchar2, p_update in boolean)
+  procedure trata_divergentes(
+    p_tabela      in varchar2,
+    p_coluna      in varchar2,
+    p_coluna_qt   in varchar2,
+    p_coluna_id   in varchar2,
+    p_usuario_id  in number, 
+    p_log_name    in varchar2, 
+    p_update      in boolean
+  )
   is
     v_valor_atual number := 0;
     v_valor_calc  number := 0;
     v_ultimo_id_atual number;
     v_ultimo_id_calc  number;
   begin
-    select u.vendas, u.ultima_venda
+    execute immediate 'select '||p_coluna_qt||', '||p_coluna_id||' from usuarios where id = :id'
     into v_valor_atual, v_ultimo_id_atual
-    from usuarios u
-    where u.id = p_usuario_id;
+    using p_usuario_id;
     
-    v_valor_calc := get_vendas_calc(p_usuario_id, v_ultimo_id_calc);
+    v_valor_calc := get_calc(p_tabela, p_coluna, p_usuario_id, v_ultimo_id_calc);
     
     if (v_valor_atual <> v_valor_calc or nvl(v_ultimo_id_atual, -1) <> nvl(v_ultimo_id_calc, -2)) then
       insert into usuarios_stats_logs (usuario_id, log_name, campo, valor_atual, ultimo_id_atual, valor_calc, ultimo_id_calc)
-      values (p_usuario_id, p_log_name, 'vendas', v_valor_atual, v_ultimo_id_atual, v_valor_calc, v_ultimo_id_calc);
+      values (p_usuario_id, p_log_name, p_tabela, v_valor_atual, v_ultimo_id_atual, v_valor_calc, v_ultimo_id_calc);
       
       if p_update then
-        update usuarios set
-          vendas = v_valor_calc,
-          ultima_venda = v_ultimo_id_calc
-        where id = p_usuario_id;
+        execute immediate 'update usuarios set '||p_coluna_qt||' = :valor_calc, '||p_coluna_id||' = :ultimo_id_calc where id = :id'
+        using v_valor_calc, v_ultimo_id_calc, p_usuario_id;
       end if;
     end if;
   exception 
@@ -56,14 +64,28 @@ create or replace package body plsql_tutorial.usuarios_stats as
   procedure audit_vendas(p_usuario_id in number)
   is
   begin
-    trata_vendas_divergentes(p_usuario_id, 'audit_vendas', false);
+    trata_divergentes('vendas', 'vendedor_id', 'vendas', 'ultima_venda', p_usuario_id, 'audit_vendas', false);
   end;
   
   
   procedure recalc_vendas(p_usuario_id in number)
   is
   begin
-    trata_vendas_divergentes(p_usuario_id, 'recalc_vendas', true);
+    trata_divergentes('vendas', 'vendedor_id', 'vendas', 'ultima_venda', p_usuario_id, 'recalc_vendas', true);
+  end;
+  
+  
+  procedure audit_ligacoes(p_usuario_id in number)
+  is
+  begin
+    trata_divergentes('ligacoes', 'atendente_id', 'ligacoes', 'ultima_ligacao', p_usuario_id, 'audit_ligacoes', false);
+  end;
+  
+  
+  procedure recalc_ligacoes(p_usuario_id in number)
+  is
+  begin
+    trata_divergentes('ligacoes', 'atendente_id', 'ligacoes', 'ultima_ligacao', p_usuario_id, 'recalc_ligacoes', true);
   end;
   
 end;
